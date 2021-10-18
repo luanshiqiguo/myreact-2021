4080f4bd8231e92981b71cd671f6bacbcbaca177
luanshiqiguo <30831656+luanshiqiguo@users.noreply.github.com>
2021-10-18 15:22:43 +0800

    Add files via upload

--- src/pages/NotOpen/index.js
+++ src/pages/NotOpen/index.js
@@ -5,18 +5,19 @@ function NotOpen() {
   return (
     <div className="not-open">
       <div className="title">
-        欢迎来到 <strong> cyc </strong> 的七维空间!
+        欢迎来到<strong>cyc</strong>的七维空间!
       </div>
       <div className="content">
-        <h1> 网站维护中， 敬请期待~ </h1>
+        <h1>网站维护中，敬请期待~</h1>
         <img src={Sorry} className="content-pic" />
       </div>
       <div className="bottom">
-        <p> creat by cyc </p>
+        <p>creat by cyc</p>
         <a
           className="bottom-code"
           href="https://beian.miit.gov.cn/#/Integrated/index"
           target="_blank"
+          rel="noreferrer"
         >
           京ICP备2021023022号
         </a>
