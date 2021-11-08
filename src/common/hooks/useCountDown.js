68ac6eaae7e0f1431062ac7461b78c40fcc8c9db
luanshiqiguo <1484836966@qq.com>
2021-11-08 18:56:18 +0800

    代码格式化

--- src/common/hooks/useCountDown.js
+++ src/common/hooks/useCountDown.js
@@ -1,20 +1,21 @@
 import { useState, useEffect, useRef } from 'react'
+import { numAddZero } from '../tool'
 
 export default function useCountDown(seconds = 0, onEnd = _ => _) {
   const hour = Math.floor(seconds / 3600)
   const minute = Math.floor((seconds - hour * 3600) / 60)
   const second = (seconds - hour * 3600 - minute * 60)
   const [timeLeft, setTimeLeft] = useState({
-    hour: hour < 10 ? '0' + hour : hour,
-    minute: minute < 10 ? '0' + minute : minute,
-    second: second < 10 ? '0' + second : second
+    hour: numAddZero(hour),
+    minute: numAddZero(minute),
+    second: numAddZero(second)
   })
   const timer = useRef(null)
 
   const init = () => {
-    const date = new Date();
-    const endTime = date.setSeconds(date.getSeconds() + Number(seconds));
-    countFun(endTime);
+    const date = new Date()
+    const endTime = date.setSeconds(date.getSeconds() + Number(seconds))
+    countFun(endTime)
   }
 
   const countFun = (endTime) => {
@@ -22,20 +23,20 @@ export default function useCountDown(seconds = 0, onEnd = _ => _) {
     timer.current = setInterval(() => {
       // 防止倒计时出现负数
       if (time > 0) {
-        time -= 1000;
+        time -= 1000
         const hour = Math.floor((time / 1000 / 3600) % 24)
         const minute = Math.floor((time / 1000 / 60) % 60)
         const second = Math.floor((time / 1000) % 60)
         setTimeLeft({
-          hour: hour < 10 ? '0' + hour : hour,
-          minute: minute < 10 ? '0' + minute : minute,
-          second: second < 10 ? '0' + second : second,
+          hour: numAddZero(hour),
+          minute: numAddZero(minute),
+          second: numAddZero(second)
         });
-      } else clearInterval(timer);
+      } else clearInterval(timer)
       // 时间结束，调用方法
       if (time === 0) onEnd()
-    }, 1000);
-  };
+    }, 1000)
+  }
 
   useEffect(() => {
     init()
---
+++ src/common/tool.js
@@ -0,0 +1,3 @@
+export const numAddZero = (num) => { // 小于10的数补0
+  return num < 10 ? '0' + num : num
+}
